8dab6e8cbd8b16a702d640b38d6826a4ed0cc3a5
Xavier Padro <xpadro.sw@gmail.com>
2020-01-21 22:19:22 +0100

    first commit

---
+++ src/main/java/com/xpadro/consumercontracttest/ConsumerContractTestApplication.java
@@ -0,0 +1,13 @@
+package com.xpadro.consumercontracttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerContractTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerContractTestApplication.class, args);
+	}
+
+}
